bc536ef6547cb7a3fe1e548d4b90f9d08a6321ee
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2024-12-24 11:08:33 +0630

    First Commit Inventory Management System

---
+++ src/inventory/Inventory.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventory;
+
+/**
+ *
+ * @author CIFIT
+ */
+public class Inventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Delegate to splash screen's functionality
+        splash_screen.main(args);
+    }
+    
+}
